907ad965da38f8e4dfbb3ab63981cbb0efde0bf6
DarioBosco <dario_bosco@protonmail.com>
2021-09-20 14:16:41 +0200

    Primo commit

---
+++ src/assets/data/links.js
@@ -0,0 +1,189 @@
+const headerLinks = [
+	{
+		text: 'Characters',
+		link: '#',
+		current: false,
+	},
+	{
+		text: 'Comics',
+		link: '#',
+		current: false,
+	},
+	{
+		text: 'Movies',
+		link: '#',
+		current: false,
+	},
+	{
+		text: 'Tv',
+		link: '#',
+		current: false,
+	},
+	{
+		text: 'Games',
+		link: '#',
+		current: false,
+	},
+	{
+		text: 'Collectibles',
+		link: '#',
+		current: false,
+	},
+	{
+		text: 'Videos',
+		link: '#',
+		current: false,
+	},
+	{
+		text: 'Fans',
+		link: '#',
+		current: false,
+	},
+	{
+		text: 'News',
+		link: '#',
+		current: false,
+	},
+	{
+		text: 'Shop',
+		link: '#',
+		current: false,
+	},
+];
+
+const footerLinks = [
+	{
+		category: 'DC COMICS',
+		items: [
+			{
+				text: 'Characters',
+				link: '#',
+			},
+			{
+				text: 'Comics',
+				link: '#',
+			},
+			{
+				text: 'Movies',
+				link: '#',
+			},
+			{
+				text: 'Tv',
+				link: '#',
+			},
+			{
+				text: 'Games',
+				link: '#',
+			},
+			{
+				text: 'Collectibles',
+				link: '#',
+			},
+			{
+				text: 'Videos',
+				link: '#',
+			},
+			{
+				text: 'Fans',
+				link: '#',
+			},
+			{
+				text: 'Fans',
+				link: '#',
+			},
+			{
+				text: 'News',
+				link: '#',
+			},
+		],
+	},
+	{
+		category: 'SHOP',
+		items: [
+			{
+				text: 'Shop DC',
+				link: '#',
+			},
+			{
+				text: 'Shop DC Collectibles',
+				link: '#',
+			},
+		],
+	},
+	{
+		category: 'DC',
+		items: [
+			{
+				text: 'Terms of Use',
+				link: '#',
+			},
+			{
+				text: 'Privacy Policy (New)',
+				link: '#',
+			},
+			{
+				text: 'Ad Choices',
+				link: '#',
+			},
+			{
+				text: 'Advertising',
+				link: '#',
+			},
+			{
+				text: 'Jobs',
+				link: '#',
+			},
+			{
+				text: 'Subscriptions',
+				link: '#',
+			},
+			{
+				text: 'Talent Workshops',
+				link: '#',
+			},
+			{
+				text: 'CPCS Certificates',
+				link: '#',
+			},
+			{
+				text: 'Ratings',
+				link: '#',
+			},
+			{
+				text: 'Shop Help',
+				link: '#',
+			},
+			{
+				text: 'Contact Us',
+				link: '#',
+			},
+		],
+	},
+	{
+		category: 'SITES',
+		items: [
+			{
+				text: 'DC',
+				link: '#',
+			},
+			{
+				text: 'MAD Magazine',
+				link: '#',
+			},
+			{
+				text: 'DC Kids',
+				link: '#',
+			},
+			{
+				text: 'DC Universe',
+				link: '#',
+			},
+			{
+				text: 'DC Power Visa',
+				link: '#',
+			},
+		],
+	},
+];
+
+export { headerLinks, footerLinks };
---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="header">
+		<div class="logo">
+			<img src="@/assets/img/dc-logo.png" alt="" />
+		</div>
+		<nav>
+			<ul>
+				<li v-for="(link, index) in links" :key="index"></li>
+			</ul>
+		</nav>
+	</div>
+</template>
+
+<script>
+import headerLinks from '@/assets/data/links.js';
+export default {
+	name: 'Header',
+	data() {
+		return {
+			links: headerLinks,
+		};
+	},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.header {
+	display: flex;
+}
+ul {
+	list-style: none;
+	background: orange;
+	li {
+		display: inline-block;
+		background: orangered;
+	}
+}
+</style>
