ef5b1ecd733eee8b1f515e9cae08401aade72e69
DarioBosco <dario_bosco@protonmail.com>
2021-09-21 18:32:20 +0200

    Footer top completato

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,21 +1,88 @@
 <template>
-	<div>
-		<div class="container-fluid">
-			hh
+	<div class="footer">
+		<div class="footer-top">
+			<div class="footer-links">
+				<ul class="footer-links-list" v-for="(link, index) in links" :key="index">
+					<li class="list-title">{{ link.category }}</li>
+					<li class="list-item" v-for="(item, index) in link.items" :key="index">
+						<a class="list-link" :href="item.link">{{ item.text }}</a>
+					</li>
+				</ul>
+			</div>
+			<div class="image-box">
+				<img src="../assets/img/dc-logo-bg.png" alt="" />
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+import footerLinks from '@/assets/data/links.js';
 export default {
-	name: 'Footer',
+	name: 'Header',
+	data() {
+		return {
+			links: footerLinks.footerLinks,
+		};
+	},
 };
 </script>
 
 <style lang="scss" scoped>
-.container-fluid {
-	height: 300px;
+.footer {
+	height: 500px;
 	width: 100vw;
-	background-image: url('@/assets/img/footer-bg.jpg');
+	background-image: url('../assets/img/footer-bg.jpg');
+	background-size: cover;
+
+	.footer-top {
+		width: 80%;
+		height: 100%;
+		margin: 0 auto;
+		color: white;
+		display: flex;
+		justify-content: space-between;
+		.footer-links {
+			width: 30%;
+			height: 100%;
+			display: flex;
+			flex-direction: column;
+			flex-wrap: wrap;
+
+			.footer-links-list {
+				width: 30%;
+
+				.list-title {
+					font-size: 1.5rem;
+					margin: 20px 0;
+					font-weight: bold;
+				}
+				.list-item {
+					margin: 0.3rem;
+					color: grey;
+				}
+			}
+		}
+		.image-box {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			width: 50%;
+			height: 100%;
+			overflow: hidden;
+			img {
+				max-width: 100%;
+				height: 160%;
+			}
+		}
+	}
+}
+
+ul {
+	list-style: none;
+}
+a {
+	color: inherit;
+	text-decoration: none;
 }
 </style>
